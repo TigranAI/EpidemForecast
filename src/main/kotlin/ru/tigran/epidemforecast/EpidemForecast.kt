8fc22611f1535b5cd4d1d2419fd2e817c6aa18ba
Tigran Shaginyan <68780335+TigranAI@users.noreply.github.com>
2024-02-13 00:21:08 +0300

    Startup script

--- src/main/kotlin/ru/tigran/epidemforecast/EpidemForecast.kt
+++ src/main/kotlin/ru/tigran/epidemforecast/EpidemForecast.kt
@@ -1,6 +1,23 @@
 package ru.tigran.epidemforecast
 
+import jade.core.Profile
+import jade.core.ProfileImpl
+import jade.core.Runtime
+import ru.tigran.epidemforecast.infected.Infected
+import ru.tigran.epidemforecast.susceptible.Susceptible
+import java.util.*
+
 class EpidemForecast
 
 fun main(args: Array<String>) {
+    val runtime = Runtime.instance()
+    val profile = ProfileImpl().apply {
+        setParameter(Profile.MAIN_HOST, "localhost")
+        setParameter(Profile.GUI, "true")
+    }
+    val containerController = runtime.createMainContainer(profile)
+    for (i in 1..1000) {
+        Susceptible.create(UUID.randomUUID().toString(), containerController)
+    }
+    Infected.create(UUID.randomUUID().toString(), containerController)
 }
