0f769029eabba314c641ad42ff22130634f74f3b
Tigran Shaginyan <68780335+TigranAI@users.noreply.github.com>
2024-02-13 00:18:32 +0300

    Init Project

---
+++ src/main/kotlin/ru/tigran/epidemforecast/EpidemForecast.kt
@@ -0,0 +1,6 @@
+package ru.tigran.epidemforecast
+
+class EpidemForecast
+
+fun main(args: Array<String>) {
+}
---
+++ src/test/kotlin/ru/tigran/epidemforecast/EpidemForecastTest.kt
@@ -0,0 +1,13 @@
+package ru.tigran.epidemforecast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EpidemForecastTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
